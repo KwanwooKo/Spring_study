2c67b3cefd31711fe74a7ec88d13230cf4d46619
고관우 <kwko97@hanyang.ac.kr>
2023-01-23 21:39:57 +0900

    memory 상으로 주문, 할인, 회원 실행 및 테스트

---
+++ study/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 동시성 issue 가 발생할 수 있지만 그냥 예제니까 HashMap 사용
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
