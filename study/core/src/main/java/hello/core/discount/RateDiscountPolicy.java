dd612dc82b4a81f7d32f1bd524d0516bbb579dc0
고관우 <kwko97@hanyang.ac.kr>
2023-01-24 17:44:43 +0900

    할인 정책 변경 및 Test 코드 추가

---
+++ study/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    // 할인 되는 금액을 반환
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
--- study/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ study/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -4,9 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class OrderServiceTest {
