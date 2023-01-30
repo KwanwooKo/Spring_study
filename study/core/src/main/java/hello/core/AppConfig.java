96c0a2751592a816aaa47cb9011ecc0561e3cd8f
고관우 <kwko97@hanyang.ac.kr>
2023-01-30 23:50:45 +0900

    AppConfig Spring 적용

--- study/core/src/main/java/hello/core/AppConfig.java
+++ study/core/src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 이렇게 AppConfig 를 사용하면서 좋아지는 점 => 사용 영역의 코드를 아예 수정하지 않아도 작동한다, 구성 영역의 코드만 수정하면 됨
+@Configuration  // 설정정보
 public class AppConfig {
 
+    /*
+     * @Bean => method 에서 반환된 객체를 spring container 에 저장 (singleton 방식으로)
+     */
+
+    @Bean
     public MemberService memberService() {
         // 역할을 사용하고, 구현이 역할 안에 들어가게끔 설정
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
--- study/core/src/main/java/hello/core/MemberApp.java
+++ study/core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // ApplicationContext => spring container
+        // AnnotationConfigApplicationContext => annotation 기반의 spring container 들
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // getBean("bean 에서 설정한 이름", 가져다 쓸 class)
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
