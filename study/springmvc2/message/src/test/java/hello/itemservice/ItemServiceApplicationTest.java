c17a24d7fd3acc5d68834582462afa85c1b2dae2
고관우 <kwko97@hanyang.ac.kr>
2023-08-02 00:18:22 +0900

    section3 message, 국제화까지 완료

---
+++ study/springmvc2/message/src/test/java/hello/itemservice/ItemServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package hello.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
