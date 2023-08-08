d6e2c9f665e820983fc4aa865c9948140b0f2522
고관우 <kwko97@hanyang.ac.kr>
2023-08-08 17:36:47 +0900

    준영속 상태

--- study/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -17,10 +17,15 @@ public class JpaMain {
         try {
 
             // 영속
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            em.flush();
+            // 특정 entity만 준영속 상태로 전환
+//            em.detach(member);          // trx commit할 때 반영하지 않음 => update query가 나가지 않아
+
+            em.clear();                   // 영속성 context를 전체 초기화
+
+            Member member2 = em.find(Member.class, 150L);       // 이 경우에 find query 다시 나옴
 
             System.out.println("=======================");
             trx.commit();
