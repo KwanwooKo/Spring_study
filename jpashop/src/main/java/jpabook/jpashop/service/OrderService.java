38eb1ee502832642a168eb6e7dcff5612cfba15c
고관우 <kwko97@hanyang.ac.kr>
2023-08-12 00:19:35 +0900

    주문 기능 테스트

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderService {
     /**
