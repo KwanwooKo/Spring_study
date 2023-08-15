5c529632a0aac8dc9dfb09bff462a0d639addcdd
고관우 <kwko97@hanyang.ac.kr>
2023-08-15 22:43:23 +0900

    지연 로딩(LAZY), 영속성 전이(CASCADE)

--- study/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ study/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,2 +1,37 @@
-package jpabook.jpashop;public class JpaMain {
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction trx = em.getTransaction();
+        trx.begin();
+
+        try {
+
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+            em.persist(order);
+
+            trx.commit();
+        } catch (Exception e) {
+            trx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
 }
--- study/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ study/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,2 +1,19 @@
-package jpabook.jpashop.domain;public class Delivery {
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
 }
