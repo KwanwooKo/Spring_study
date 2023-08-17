e970469dc4e560aae07d4becf418b6d1c591b886
고관우 <kwko97@hanyang.ac.kr>
2023-08-17 22:33:49 +0900

    값 타입 Collection 지연 로딩

--- study/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,7 +18,6 @@ public class JpaMain {
         try {
 
             Member member = new Member();
-
             member.setUsername("member1");
             member.setHomeAddress(new Address("homeCity", "street", "10000"));
 
@@ -28,6 +29,23 @@ public class JpaMain {
             member.getAddressHistory().add(new Address("old2", "street", "10000"));
 
             em.persist(member);
+
+            // DB에만 존재, 메모리에는 없어
+            em.flush();
+            em.clear();
+
+            System.out.println("====================== START ======================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address.getCity() = " + address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
 
             trx.commit();
         } catch (Exception e) {
