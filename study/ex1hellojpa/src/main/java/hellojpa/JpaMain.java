b10189148ec783c0ff2bbf328e39c1666a2f75ae
고관우 <kwko97@hanyang.ac.kr>
2023-08-18 18:15:27 +0900

    criteria builder -> 실무에서는 못 씀(유지보수 불가능)

--- study/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ study/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -17,48 +20,16 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            // Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
+            Root<Member> m = query.from(Member.class);
 
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
 
-            em.persist(member);
-
-            // DB에만 존재, 메모리에는 없어
-            em.flush();
-            em.clear();
-
-            System.out.println("====================== START ======================");
-            Member findMember = em.find(Member.class, member.getId());
-
-            /**
-             * 값 타입 Collection은 언제 써?
-             * 값을 더이상 추적할 필요가 없는 데이터의 경우
-             * 그러면 주소는 뭘 써야될까?
-             * -> 무조건 Entity
-             *
-             * 값 타입은 값 타입이라 판단될 때만 사용해야돼
-             */
-
-
-            // homeCity -> newCity
-//            findMember.getHomeAddress().setCity("newCity");       // 절대 이렇게 쓰면 안돼
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");     // Collection의 값만 변경되어도 db에서 실제값이 같이 변경돼
-//            findMember.getFavoriteFoods().add("한식");
-
-            // equals, hashCode 로 삭제해
-//            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
+            List<Member> resultList = em.createQuery(cq)
+                    .getResultList();
 
             trx.commit();
         } catch (Exception e) {
